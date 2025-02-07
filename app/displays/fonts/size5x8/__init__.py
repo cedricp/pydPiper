111b2cbc62bab4195f00cbe23c0576714cce5ce9
cedric paillé <cedricpaille@gmail.com>
2025-02-07 09:31:35 +0100

    fix import #2

--- app/displays/fonts/size5x8/__init__.py
+++ app/displays/fonts/size5x8/__init__.py
@@ -1,62 +1,15 @@
 __all__ = [ "player", "playing", "repeat_all", "repeat_once", "shuffle", "speaker", "volume", "system", "bigclock", "bigchars", "bigplay", "latin1" ]
 
 
-try:
-	import player
-except ImportError:
-	pass
-
-try:
-	import playing
-except ImportError:
-	pass
-
-try:
-	import repeat_all
-except ImportError:
-	pass
-
-try:
-	import repeat_once
-except ImportError:
-	pass
-
-try:
-	import shuffle
-except ImportError:
-	pass
-
-try:
-	import speaker
-except ImportError:
-	pass
-
-try:
-	import volume
-except ImportError:
-	pass
-
-try:
-	import system
-except ImportError:
-	pass
-
-try:
-	import bigclock
-except ImportError:
-	pass
-
-try:
-	import bigchars
-except ImportError:
-	pass
-
-try:
-	import bigplay
-except ImportError:
-	pass
-
-try:
-	import latin1
-except ImportError:
-	pass
+import player
+import playing
+import repeat_all
+import repeat_once
+import shuffle
+import speaker
+import volume
+import system
+import bigclock
+import bigchars
+import bigplay
+import latin1
